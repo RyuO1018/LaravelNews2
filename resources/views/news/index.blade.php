ed46f2f7efe776483029905ba73ae27886c83820
Ryuta Omoto <ryuo1018birth@gmail.com>
2020-05-11 12:01:42 +0000

    8th commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -26,6 +26,7 @@
                 </div>
             </div>
         @endif
+        <p>ちくわ大明神</p>
         <hr color="#c0c0c0">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
